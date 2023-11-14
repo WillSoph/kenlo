4876fd2394743b46259def6282a2fca61d51502a
Willians Ferreira <williansdesigner@hotmail.com>
2023-11-14 09:48:28 -0300

    correção no env.local

--- main-app/pages/catalogOnMain.js
+++ main-app/pages/catalogOnMain.js
@@ -1,5 +1,5 @@
 import dynamic from 'next/dynamic';
  
-const Page = dynamic(() => import('shop/pages/catalog'), { ssr: false});
+const Page = dynamic(() => import('shop/catalog'), { ssr: false});
  
 export default Page;
